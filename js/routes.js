504c574234ac67c4d91fe1cf424d65e9da40ab61
ignDoom <pata150@email.cz>
2024-09-21 14:31:46 +0200

    add submitting recordds
    yeah

--- js/routes.js
+++ js/routes.js
@@ -2,10 +2,12 @@ import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import ListPacks from './pages/ListPacks.js'
+import Submit from './pages/Submit.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
     { path: '/list-packs', component: ListPacks },
+    { path: '/submit', component: Submit },
 ];
