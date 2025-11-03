efd21f9aabeb2a5c52ae2034b1ed18a5d8778072
Catty <59940789+swifticat@users.noreply.github.com>
2025-11-03 11:34:26 +1100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -201,7 +201,7 @@ export default {
     }),
     computed: {
         level() {
-            return this.list[this.selected][0];
+            return this.list[this.selected][1];
         },
     },
     async mounted() {
