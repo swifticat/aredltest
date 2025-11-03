c31c316f87bb2a5d80761baf93a1c0766632a608
Catty <59940789+swifticat@users.noreply.github.com>
2025-11-03 11:17:12 +1100

    Update ListPacks.js

--- js/pages/ListPacks.js
+++ js/pages/ListPacks.js
@@ -70,6 +70,7 @@ export default {
                             </td>
                             <td class="hz">
                                 <p>{{ record.hz }}Hz</p>
+                            </td>
                             <td class="enjoyment">
                                 <p>{{ record.enjoyment }}Enjoyment</p>                                
                             </td>
