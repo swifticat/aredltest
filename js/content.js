6b07b3e7588b383380ae535664076b8f71c36cb9
Catty <59940789+swifticat@users.noreply.github.com>
2025-11-03 11:09:51 +1100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -123,7 +123,6 @@ export async function fetchLeaderboard() {
                 rank: rank + 1,
                 level: level.name,
                 percent: record.percent,
-                enjoyment: record.enjoyment,
                 score: score(rank + 1, record.percent, level.percentToQualify, list.length),
                 link: record.link,
                 path: level.path,
